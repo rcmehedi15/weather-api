c2838a55c694e6d6a2e544c8e2d12c0c037f1e1b
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-03-05 22:12:34 +0600

    input field and button design

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
 const API_KEY = `cccd20f55b6d176a96be108ca888b43f`;
-const loadTemeratur = city => {
+
+// api data fetching 
+const loadTemperature = city => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayTemperature(data))
 }
-loadTemeratur("Dhaka")
+
+    //  click button 
+document.getElementById('btn-search').addEventListener('click', function(){
+    // search field input value
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // set city 
+    document.getElementById('city').innerText = city;
+    loadTemperature(city);
+})
+// temperature display , data value store
+const displayTemperature = data => {
+    const temperature = document.getElementById('temperature');
+    temperature.innerText = data.main.temp;
+    
+}
+
+
+
+loadTemperature("Dhaka")
